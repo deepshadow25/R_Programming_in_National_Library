c2ca520a707da5ee743b18330e7cfbc25ad8e60b
Seongyeon <115054681+deepshadow25@users.noreply.github.com>
2023-04-13 14:18:41 +0900

    Update 230413.R

--- 230413.R
+++ 230413.R
@@ -1,8 +1,11 @@
 # 인사관리 데이터를 통한 R 데이터 핸들링 [1]
 
+getwd() # 현재 디렉토리 확인하기
+
 # 데이터 불러오기
 DATA = read.csv('C:/Users/NDL/Documents/수업자료/HR_comma_sep.csv')
 
 head(DATA) # 데이터 윗부분 띄우기
 str(DATA) # 데이터 strings 파악
 summary(DATA) # 요약된 데이터 살펴보기
+
