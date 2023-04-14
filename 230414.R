1f983d484a0496365258e049658809cc89cd32a3
Seongyeon <115054681+deepshadow25@users.noreply.github.com>
2023-04-14 14:31:11 +0900

    Update 230414.R

--- 230414.R
+++ 230414.R
@@ -1,2 +1,18 @@
-# 4월 14일 수업
+# 230414
+# 4월 13일 수업에 이어서 진행
 
+# salary_mid, sales
+
+DATA_mid = subset(DATA, salary == 'medium')
+DATA_sales = subset(DATA, sales =='sales')
+
+DATA_Mid_Sales = subset(DATA, salary == 'medium' & sales == 'sales')
+remove(DATA_Mid_Sales)
+
+
+# 집계된 데이터를 생성
+
+install.packages('plyr')
+library(plyr)
+
+ss = ddply()
